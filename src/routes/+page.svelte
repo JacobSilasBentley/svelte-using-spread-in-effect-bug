e15b201e22c1df9cc22a277073526d20af6368a8
Jacob Silas Bentley <bentley.silas@gmail.com>
2024-05-05 10:35:12 +0100

    Recreated issue in template version of svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let value = $state({ options:[] });
+    let other = $state('');
+
+    $effect(() => {
+        console.log("Running Effect")
+        value = { options:[] };
+        other = [...value.options];
+    })
+</script>
+
+<div>Hello</div>
